7a34dec44f82eddc89ccc6b642afd032cbcb4cb6
Raditya Catur Narendra <2041720198@student.polinema.ac.id>
2023-10-09 09:34:46 +0700

    install sequelize + mysql2 and importing

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
+// import
 var express = require('express');
-var app = express();
 var path = require('path');
+var connection = require('./app/model/index');
+var app = express();
 
-app.get('/api/data', function(req, res) {
-    res.json({ message: 'Reading data through GET request' });
-});
-
-app.post('/api/data', function(req, res) {
-    res.json({ message: 'Creating data through GET request' });
-});
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// routes
 app.get('/', async(req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
@@ -24,6 +21,23 @@ app.get('/profiles', async(req, res) => {
     res.sendFile(path.join(__dirname, 'pages', 'profiles.html'));
 });
 
-app.listen(3000, () => {
+// functions
+app.get('/api/data', function(req, res) {
+    res.json({ message: 'Reading data through GET request' });
+});
+
+app.post('/api/data', function(req, res) {
+    res.json({ message: 'Creating data through GET request' });
+});
+
+app.listen(3000, function() {
     console.log("Server is running succesfully on port 3000");
+    connection.authenticate()
+    .then(function() {
+        console.log("Database terhubung");
+    })
+    .catch(function(error) {
+        console.log("Error terhubung ke Database", error);
+        process.exit();
+    })
 });
